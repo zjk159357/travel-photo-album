19f47668ae3c9a20630e440d16c595438d7081fe
钟佳楷 <3552219514@qq.com>
2025-10-01 10:40:21 +0800

    添加相册功能

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// 自动导入 src/assets/images 下的所有图片
+const imageImports = import.meta.glob('./assets/images/*.{jpg,png,jpeg}', { eager: true });
+const imagePaths = Object.keys(imageImports).map(key => imageImports[key].default);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  return (
+    <div className="album">
+      <h1>我的旅行相册</h1>
+      <div className="photo-grid">
+        {imagePaths.map((src, index) => (
+          <div key={index} className="photo-card">
+            <img 
+              src={src} 
+              alt={`旅行照片 ${index + 1}`} 
+              style={{ width: '300px', height: '200px', objectFit: 'cover' }} 
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
